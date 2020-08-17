f528e8e5e27ec3ef078165da9cb42509fe791ba6
Lusaiwen <1811977648@qq.com>
2020-08-17 16:50:35 +0800

    img-water

--- routes/api/upload.js
+++ routes/api/upload.js
@@ -2,12 +2,38 @@ const express = require("express")
 const router = express.Router();
 const multer = require("multer")
 const path = require("path")
+const jimp = require("jimp");
+const { async } = require("validate.js");
+
+async function water(originPath, waterPath, targetPath, proportion = 10, marginProportion = 0.05) {
+    const [water, origin] = await Promise.all([
+        jimp.read(waterPath),
+        jimp.read(originPath)
+    ])
+
+    // 水印图片缩放
+    const curProportion = origin.bitmap.width / water.bitmap.width; 
+    water.scale(curProportion / proportion);
+
+
+    //计算位置
+    const right = origin.bitmap.width * marginProportion;
+    const bottom = origin.bitmap.height * marginProportion;
+    const x = origin.bitmap.width - water.bitmap.width - right;
+    const y = origin.bitmap.height - water.bitmap.height - bottom;
+
+    origin.composite(water,x, y, {
+        mode: jimp.BLEND_SOURCE_OVER,
+        opacitySource: 0.5,
+    });
+    await origin.write(targetPath);
+}
 
 
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-        cb(null, path.resolve(__dirname, "../../public/upload"))
+        cb(null, path.resolve(__dirname, "../../public/origin"))
     },
     filename: function (req, file, cb) {
         const timeStamp = Date.now();
@@ -21,39 +47,33 @@ const storage = multer.diskStorage({
 const upload = multer({
     storage,
     limits: {
-        fileSize: 150 * 1024
+        // fileSize: 100 * 1024
     },
     fileFilter (req, file, cb) {
         const whiteList = [".jpg", ".gif", ".png", ".jpeg"];
         const ext = path.extname(file.originalname);
-        if(whiteList.includes(ext)){
+        if(whiteList.includes(ext.toLowerCase())){
             cb(null, true);
         }else {
             cb(new Error(`your ext name of ${ext} is not support`))
         }
-        // 这个函数应该调用 `cb` 用boolean值来
-        // 指示是否应接受该文件
-      
-        // 拒绝这个文件，使用`false`，像这样:
-        cb(null, false)
-      
-        // 接受这个文件，使用`true`，像这样:
-        cb(null, true)
-      
-        // 如果有问题，你可以总是这样发送一个错误:
-        cb(new Error('I don\'t have a clue!'))
       
       }
 })
 
 
 
-router.post("/", upload.single("img"), (req, res) => {
+router.post("/", upload.single("img"),async (req, res) => {
     const url = `/upload/${req.file.filename}`;
+    const waterPath = path.resolve(__dirname, "../../public/img/water.jpg")
+    //加水印
+    const targetPath = path.resolve(__dirname, "../../public/upload", req.file.filename)
+    await water(req.file.path, waterPath, targetPath);
+    
     res.send({
         code: 0,
         msg: "",
-        url
+        data: url
     })
 })
 
